28fef83fcae6871c1f194e99f083c49528281e77
anthony <anthonycakins@gmail.com>
2023-05-12 11:01:13 +1000

    ecom--app

---
+++ app/product/[id]/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="text-center ">
+        <h2 className="font-bold">404 Not found</h2>
+        <p className="font-bold">Could not find requested resource</p>
+      </div>
+    </div>
+  );
+}
